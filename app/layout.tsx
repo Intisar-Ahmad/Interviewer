f96a76a4ac655ee1f22e4f18dd4a453b30a8dfcb
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-30 19:18:02 +0500

    Initializing the project. Getting things setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${monaSans.variable} antialiased pattern`}
       >
         {children}
       </body>
